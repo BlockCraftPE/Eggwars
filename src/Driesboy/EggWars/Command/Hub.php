94f7b726596ac5f134c528604338485f17e9a145
Driesboy <coenendries3@gmail.com>
2017-04-14 07:51:23 +0200

    Close #4

--- src/Driesboy/EggWars/Command/Hub.php
+++ src/Driesboy/EggWars/Command/Hub.php
@@ -17,14 +17,14 @@ class Hub extends Command{
 
     public function execute(CommandSender $g, $label, array $args){
         $main = EggWars::getInstance();
-        if($main->oyuncuArenadami($g->getName())){
+        if($main->IsInArena($g->getName())){
             $arena = $main->IsInArena($g->getName());
             $ac = new Config($main->getDataFolder()."Arenas/$arena.yml", Config::YAML);
             $durum = $ac->get("Status");
             if($durum === "Lobby"){
                 $main->RemoveArenaPlayer($arena, $g->getName());
                 $g->teleport(Server::getInstance()->getDefaultLevel()->getSafeSpawn());
-                $g->sendMessage("§8» §aYou are teleported to the lobby");
+                $g->sendMessage("§8» §aYou are teleported to the Lobby");
             }else{
                 $g->sendMessage("§8» §cYour game is running");
             }
